495a1c4d4655c36bb3d9d5ba04cfc7a507e499a7
unknown <kalyaninalamaru.com>
2023-12-15 17:46:38 +0530

    Variable code

---
+++ 01variable.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Ramesh:: Hello Suresh, Good Evening"
+echo "Suresh:: Hi Rames, Very Good Evening"
+echo "Ramesh:: How are you Suresh?"
+echo "Suresh:: Iam Good Ramesh, How are you"
+
