2f7d325e7d1fed483b976c5d07c920ba5044027c
unknown <kalyaninalamaru.com>
2023-12-15 18:12:35 +0530

    Variable code

--- 02-variable.sh
+++ 02-variable.sh
@@ -5,3 +5,6 @@ PERSON2=Rahim
 
 echo "$PERSON1 :: Hello $PERSON2, Good Morning"
 echo "$PERSON2 :: Hi $PERSON1 , Very Good Morning"
+echo "$PERSON1 :: How are you $PERSON2?"
+echo "$PERSON2 :: Iam Good $PERSON1, How are you"
+
