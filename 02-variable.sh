44fa415a65ef2f6e159dfbac33924dce96d97b23
unknown <kalyaninalamaru.com>
2023-12-15 18:02:45 +0530

    Variable code

--- 02-variable.sh
+++ 02-variable.sh
@@ -3,5 +3,5 @@
 PERSON=Ram
 PERSON=Rahim
 
-echo "$PERSON1 :: Hello $PERSON2, Good Morning"
-echo "$PERSON2 :: Hi $PERSON1 , Very Good Morning"
+echo "$ PERSON1 :: Hello $ PERSON2, Good Morning"
+echo "$ PERSON2 :: Hi $ PERSON1 , Very Good Morning"
